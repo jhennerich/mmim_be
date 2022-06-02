9411034369c81a05d405746b713302dcd4204ae7
John Hennerich <john.hennerich@gmail.com>
2022-06-02 12:01:32 -0600

    WIP: add model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  
+  has_many :user_meetings
+  has_many :meetings, through: :user_meetings
+
 end

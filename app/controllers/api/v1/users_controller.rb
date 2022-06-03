c237e23ca22918dccc6528cf6b77697cfc9abfbc
Maximilian Wagner <maw3350@gmail.com>
2022-06-03 04:15:23 -0400

    WIP: start post request for users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,12 @@
 class Api::V1::UsersController < ApplicationController
 
+  def create
+    render json: UserSerializer.new(User.create(user_params)), status: 201
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email)
+  end
 end

0aadd40fa62d1310849af9dc7cf3581bb857b66f
Kim G <xiaole.guo@yahoo.com>
2022-06-06 12:03:12 -0600

    add stub method

--- spec/requests/api/v1/search_spec.rb
+++ spec/requests/api/v1/search_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'The search API' do
 #end
   it 'midpoints search', :vcr do
     VCR.insert_cassette('The_search_API/midpoints_search', :record => :new_episodes)
-
+    allow(MidpointFacade).to receive(:find_midpoint).and_return("39.74574405%2C-104.99464955")
     params = {
       address_1: '1583 Newton St Denver CO',
       address_2: '2300 Steele St Denver CO',

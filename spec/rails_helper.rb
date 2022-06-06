6aa5dbe3980e875f9b4a064e8572fdfaee4430e1
Kim G <xiaole.guo@yahoo.com>
2022-06-06 11:22:06 -0600

    configure new_espisode on vcr

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,10 +77,7 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.default_cassette_options = {
-    :match_requests_on => [:method,
-      VCR.request_matchers.uri_without_param(:url)]
-  }
   config.filter_sensitive_data("google_api_key") { ENV['google_api_key'] }
 #  config.default_cassette_options = {re_record_interval: 1.day}
 end
+VCR.insert_cassette('test_data', :record => :new_episodes)

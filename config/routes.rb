3e69dc932b4ce4da50113e2c7e9943ac0aed8559
Will Medders <wmedders@gmail.com>
2022-06-06 13:15:05 -0400

    Feature search by user

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       put '/users', to: 'users#update'
       resources :users, only: [:create]
       resources :search, only: [:index]
+      resources :search_by_user, only: [:index]
     end
   end
 end

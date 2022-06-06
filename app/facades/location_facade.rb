259424534337fd2f971920509d8c83a49e0bc41e
John Hennerich <john.hennerich@gmail.com>
2022-06-06 16:36:54 -0600

    Feat: GoogleAPI neabysearch rateby disatance

--- app/facades/location_facade.rb
+++ app/facades/location_facade.rb
@@ -4,23 +4,9 @@ class LocationFacade
   end
 
   def self.get_near_by_locations(mid_coord, category)
-    location_results = service.nearbysearch("location=#{mid_coord}&type=#{category}&radius=1500")
+    location_results = service.nearbysearch("location=#{mid_coord}&type=#{category}&rankby=distance")
     location_results[:results].map do |location_result|
       LocationResponse.new(location_result)
     end
   end
-
-  #   def self.create_location(results)
-#     results[:results].map do |result|
-#       Location.new(
-#         id: meeting_id[:id].to_i,
-#         place_id: result[:place_id],
-#         name: result[:name],
-# #        address:
-#         rating: result[:rating],
-#         price_range: result[:price_level]
-#       )
-#     end
-#   end
-
 end

e6a8929fab34f6a3839fe4b21ba76d17de9563bd
John Hennerich <john.hennerich@gmail.com>
2022-06-06 21:01:45 -0600

    Test: build out spec test for GoogleMapApiService

--- app/services/google_map_api_service.rb
+++ app/services/google_map_api_service.rb
@@ -1,20 +1,20 @@
 class GoogleMapApiService
-  def conn
+  def self.conn
     Faraday.new(url: 'https://maps.googleapis.com/maps/api') do |faraday|
     faraday.params['key'] = ENV['google_api_key']
     end
   end
 
-  def parse_json(response)
+  def self.parse_json(response)
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def address_to_geocode(address)
+  def self.address_to_geocode(address)
     response = conn.get("geocode/json?address=#{address}")
     parse_json(response)
   end
 
-  def nearbysearch(attributes)
+  def self.nearbysearch(attributes)
     response = conn.get("place/nearbysearch/json?#{attributes}")
     parse_json(response)
   end
---
+++ spec/services/google_map_api_service_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe GoogleMapApiService do
+  describe 'happy paths' do
+    it 'can get lat and lng for a street address' do
+      address = "2300 Steele St, Denver, CO 80205, USA"
+      json_response = File.read('spec/fixtures/address_to_geocode.json')
+      stub_request(:get, "https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=#{ENV['google_api_key']}")
+      .to_return(status: 200, body: json_response, headers: {})
+
+      response = GoogleMapApiService.address_to_geocode(address)[:results]
+
+      expect(response).to be_an(Array)
+      expect(response[0][:geometry][:location]).to be_a(Hash)
+      expect(response[0][:geometry][:location]).to have_key(:lat)
+      expect(response[0][:geometry][:location][:lat]).to be_a(Float)
+      expect(response[0][:geometry][:location]).to have_key(:lng)
+      expect(response[0][:geometry][:location][:lng]).to be_a(Float)
+
+    end
+
+    it 'can get nearby locations given a midpoint' do
+      mid_coord = "39.7512038%2C-104.9447319"
+      attributes = "location=#{mid_coord}&type=cafe&rankby=distance"
+      json_response = File.read('spec/fixtures/nearbysearch.json')
+      stub_request(:get, "https://maps.googleapis.com/maps/api/place/nearbysearch/json?#{attributes}&key=#{ENV['google_api_key']}")
+      .to_return(status: 200, body: json_response, headers: {})
+
+      response = GoogleMapApiService.nearbysearch(attributes)[:results]
+
+      expect(response).to be_an(Array)
+      expect(response[0][:geometry][:location]).to be_a(Hash)
+      expect(response[0][:geometry][:location]).to have_key(:lat)
+      expect(response[0][:geometry][:location][:lat]).to be_a(Float)
+      expect(response[0][:geometry][:location]).to have_key(:lng)
+      expect(response[0][:geometry][:location][:lng]).to be_a(Float)
+      expect(response[0]).to have_key(:name)
+      expect(response[0][:name]).to be_a(String)
+      expect(response[0]).to have_key(:place_id)
+      expect(response[0][:place_id]).to be_a(String)
+      expect(response[0]).to have_key(:vicinity)
+      expect(response[0][:vicinity]).to be_a(String)
+      expect(response[0]).to have_key(:rating)
+      expect(response[0][:rating]).to be_a(Float)
+    end
+  end
+end

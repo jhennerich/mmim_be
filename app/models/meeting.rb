d75c9905143ffa707d18729c36f0c16228fd9b18
John Hennerich <john.hennerich@gmail.com>
2022-06-08 13:51:40 -0600

    Refactor: add dependent destroy to meeting modle

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -4,6 +4,6 @@ class Meeting < ApplicationRecord
   validates :host_name, presence: true
   validates :guest_name, presence: true
 
-  has_many :user_meetings
+  has_many :user_meetings, dependent: :destroy
   has_many :users, through: :user_meetings
 end

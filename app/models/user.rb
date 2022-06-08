7104576808ebe7a197e1aef0be6c5382172b04bb
John Hennerich <john.hennerich@gmail.com>
2022-06-07 20:15:06 -0600

    Method: find_all_meetings on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  
+
   has_many :user_meetings
   has_many :meetings, through: :user_meetings
 
+  def self.find_all_meetings(user_id)
+    User.joins(:meetings)
+    .where(id: "#{user_id}")
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,5 +10,20 @@ RSpec.describe User, type: :model do
   describe "relationships" do
     it { should have_many :user_meetings}
     it { should have_many :meetings}
-   end
+  end
+
+  describe "class methods" do
+    it "#find_all_meetings" do
+      host = User.create!(name: 'john', email: 'john@email.com')
+      guest = User.create!(name: 'max', email: 'max@email.com')
+      meeting_1 = Meeting.create!(place_id: "somewhere", status: 0)
+      meeting_2 = Meeting.create!(place_id: "somewhere", status: 0)
+      meeting_3 = Meeting.create!(place_id: "somewhere else", status: 0)
+
+      user_meeting = UserMeeting.create!(user_id: host.id, meeting_id: meeting_1.id, host: true)
+      user_meeting3 = UserMeeting.create!(user_id: host.id, meeting_id: meeting_2.id, host: true)
+      user_meeting2 = UserMeeting.create!(user_id: guest.id, meeting_id: meeting_1.id, host: false)
+      meeting_ids = User.find_all_meetings(host.id).first.meeting_ids
+     end
+  end
 end

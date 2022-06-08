365930c33163486e46e927ddb74dfb03f6493c7b
Kim G <xiaole.guo@yahoo.com>
2022-06-08 10:34:27 -0600

    added users to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# the following two users are for-front end testing purposes please don't delete
+User.create(name: 'usersearch base', email: 'user_search@email.com', address: '2001 Colorado Blvd, Denver, CO 80205')
+User.create(name: 'noadd user', email: 'no_add@email.com', address: nil)

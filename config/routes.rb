7c1a2489f76e8811bfba7e1b52f87f05d6c027cd
Maximilian Wagner <maw3350@gmail.com>
2022-06-08 16:14:32 -0400

    feat: add user_meetings route/controller action. Merging then pulling down updated database. Will add additional tests after.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       put '/users', to: 'users#update'
+      get 'user_meetings', to: 'user_meetings#index'
       resources :users, only: [:create]
       resources :search, only: [:index]
       resources :search_by_user, only: [:index]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+UserMeeting.destroy_all
+Meeting.destroy_all
+User.destroy_all
+User.destroy_all
+
+user_1 = User.create(name: 'Max', email: 'max@test.com', address: '963 test dr')
+user_2 = User.create(name: 'John', email: 'john@test.com', address: '852 test rd')
+user_3 = User.create(name: 'Oscar', email: 'oscar@test.com', address: '741 test way')
+user_4 = User.create(name: 'Kim', email: 'kim@test.com', address: '951 test circle')
+
+meeting_1 = Meeting.create(location_name: "A great place", location_address: "123 test way")
+meeting_2 = Meeting.create(location_name: "A bad place", location_address: "234 test blvd")
+meeting_3 = Meeting.create(location_name: "A terrible place", location_address: "345 test st")
+meeting_4 = Meeting.create(location_name: "A good place", location_address: "456 test place")
+meeting_5 = Meeting.create(location_name: "An OK place", location_address: "567 test dr")
+
+user_meeting_1 = UserMeeting.create(user_id: user_1.id, meeting_id: meeting_1.id)
+user_meeting_2 = UserMeeting.create(user_id: user_1.id, meeting_id: meeting_2.id)
+user_meeting_3 = UserMeeting.create(user_id: user_1.id, meeting_id: meeting_3.id)
+user_meeting_4 = UserMeeting.create(user_id: user_1.id, meeting_id: meeting_4.id)
+user_meeting_5 = UserMeeting.create(user_id: user_1.id, meeting_id: meeting_5.id)

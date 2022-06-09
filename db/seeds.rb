cd3432418fe7fb2489ce9236039ad07f87de0589
John Hennerich <john.hennerich@gmail.com>
2022-06-08 20:18:44 -0600

    Test: add user_meeting endpoint specs

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,11 +15,16 @@ user_2 = User.create(name: 'John', email: 'john@test.com', address: '852 test rd
 user_3 = User.create(name: 'Oscar', email: 'oscar@test.com', address: '741 test way')
 user_4 = User.create(name: 'Kim', email: 'kim@test.com', address: '951 test circle')
 
-meeting_1 = Meeting.create(location_name: "A great place", location_address: "123 test way")
-meeting_2 = Meeting.create(location_name: "A bad place", location_address: "234 test blvd")
-meeting_3 = Meeting.create(location_name: "A terrible place", location_address: "345 test st")
-meeting_4 = Meeting.create(location_name: "A good place", location_address: "456 test place")
-meeting_5 = Meeting.create(location_name: "An OK place", location_address: "567 test dr")
+meeting_1 = Meeting.create(location_name: "A great place", location_address: "123 test way",
+host_name: "john", guest_name: "max")
+meeting_2 = Meeting.create(location_name: "A bad place", location_address: "234 test blvd",
+host_name: "john", guest_name: "max")
+meeting_3 = Meeting.create(location_name: "A terrible place", location_address: "345 test st",
+host_name: "john", guest_name: "max")
+meeting_4 = Meeting.create(location_name: "A good place", location_address: "456 test place",
+host_name: "john", guest_name: "max")
+meeting_5 = Meeting.create(location_name: "An OK place", location_address: "567 test dr",
+host_name: "john", guest_name: "max")
 
 user_meeting_1 = UserMeeting.create(user_id: user_1.id, meeting_id: meeting_1.id)
 user_meeting_2 = UserMeeting.create(user_id: user_1.id, meeting_id: meeting_2.id)
@@ -31,9 +36,9 @@ user_meeting_5 = UserMeeting.create(user_id: user_1.id, meeting_id: meeting_5.id
 User.create(name: 'usersearch base', email: 'user_search@email.com', address: '2001 Colorado Blvd, Denver, CO 80205')
 User.create(name: 'noadd user', email: 'no_add@email.com', address: nil)
 
-host = User.create!(name: 'john', email: 'john@email.com')
-guest = User.create!(name: 'max', email: 'max@email.com')
-meeting_1 = Meeting.create(location_name: "John's House",
+host = User.create(name: 'john', email: 'john@email.com')
+guest = User.create(name: 'max', email: 'max@email.com')
+john_meeting = Meeting.create(location_name: "John's House",
       location_address: "1583 Newton St. Denver CO",
       host_name: "john",
       guest_name: "max"
---
+++ spec/requests/api/v1/user_meeting_request_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe 'user_meeting API endpoint' do
+  it 'returns all meeting data for a give user email' do
+    host = User.create!(name: 'john', email: 'john@email.com')
+    guest = User.create!(name: 'max', email: 'max@email.com')
+    meeting = Meeting.create!(
+      :host_name=>"john",
+      :guest_name=>"max",
+      :location_name=>"John's House",
+      :location_address =>"1583 Newton St Denver CO"
+    )
+
+    meeting_1 = Meeting.create!(location_name: "A great place", location_address: "123 test way",
+    host_name: "john", guest_name: "max")
+    meeting_2 = Meeting.create!(location_name: "A bad place", location_address: "234 test blvd",
+    host_name: "john", guest_name: "max")
+
+    user_meeting_1 = UserMeeting.create(user_id: host.id, meeting_id: meeting.id)
+    user_meeting_2 = UserMeeting.create(user_id: host.id, meeting_id: meeting_1.id)
+
+    params = { email: 'john@email.com' }.to_json
+    get "/api/v1/user_meetings", params: { params: params }
+
+    expect(response).to be_successful
+
+    john_meetings = JSON.parse(response.body, symbolize_names: true)[:data]
+    john_meetings.each do |meet|
+      expect(meet).to have_key(:id)
+      expect(meet[:id]).to be_a(String)
+      expect(meet).to have_key(:type)
+      expect(meet[:type]).to eq('meeting')
+
+      expect(meet).to have_key(:attributes)
+      expect(meet[:attributes].count).to eq(4)
+
+      expect(meet[:attributes]).to have_key(:host_name)
+      expect(meet[:attributes][:host_name]).to be_a(String)
+
+      expect(meet[:attributes]).to have_key(:guest_name)
+      expect(meet[:attributes][:guest_name]).to be_a(String)
+      expect(meet[:attributes]).to have_key(:location_name)
+      expect(meet[:attributes][:location_name]).to be_a(String)
+
+      expect(meet[:attributes]).to have_key(:location_address)
+      expect(meet[:attributes][:location_address]).to be_a(String)
+    end
+  end
+end

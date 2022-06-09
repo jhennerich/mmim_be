41e901566fec886957cb075b368412baac4e85db
John Hennerich <john.hennerich@gmail.com>
2022-06-08 18:55:37 -0600

    Test: fixing spec for circleci

--- spec/facades/location_facade_spec.rb
+++ spec/facades/location_facade_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 describe 'Location Facade' do
-  it 'returns locations given a midpoint', :vcr do
+  it 'returns locations given a midpoint' do
+    json_response = File.open("./spec/fixtures/nearbysearch2.json")
+    stub_request(:get, "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=#{ENV['google_api_key']}&location=39.7512038%2C-104.9447319&type=cafe&rankby=distance")
+    .to_return(status: 200, body: json_response)
+
     mid_coord = "39.7512038%2C-104.9447319"
-    attributes = "location=#{mid_coord}&type=cafe&rankby=distance"
-    json_response = File.read('spec/fixtures/nearbysearch.json')
-    stub_request(:get, "https://maps.googleapis.com/maps/api/place/nearbysearch/json?#{attributes}&key=#{ENV['google_api_key']}")
-    .to_return(status: 200, body: json_response, headers: {})
+  #  attributes = "location=#{mid_coord}&type=cafe&rankby=distance"
+  #  json_response = File.read('spec/fixtures/nearbysearch.json')
+#    stub_request(:get, "https://maps.googleapis.com/maps/api/place/nearbysearch/json?#{attributes}&key=#{ENV['google_api_key']}")
+#    .to_return(status: 200, body: json_response, headers: {})
 
     location_results = LocationFacade.get_near_by_locations(mid_coord, "cafe")
     expect(location_results).to be_a(Array)
